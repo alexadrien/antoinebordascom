2f69dd1ced8daa4debe8e12e9f665109df9c3a5b
Antoine Bordas <antoinebordas@MacBook-Pro-de-Antoine-6.local>
2019-08-29 18:50:43 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import image from './image.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={image} className="App-logo" alt="logo" />
         <p>
           Welcome Home
         </p>
