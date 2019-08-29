c419ed83a02eef0319cb48bf771a59332a2e6728
Antoine Bordas <antoinebordas@MacBook-Pro-de-Antoine-6.local>
2019-08-29 18:44:09 +0200

    save

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.sipios.com/blog/alliances-banque-et-fintech-finance-du-futur"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          OPM
         </a>
       </header>
     </div>
